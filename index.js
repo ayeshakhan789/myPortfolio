d9efae27331ca81fa3cf2596309426138e3494df
Ayeshakhan <ayeshakk456@gmail.com>
2024-10-24 18:24:33 +0500

    completed question 2

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// Select the hamburger icon, mobile menu, and close button
+
 const hamburger = document.getElementById('hamburger');
 const mobileMenu = document.getElementById('mobileMenu');
 const closeMenu = document.getElementById('closeMenu');
 
-// Show mobile menu when hamburger is clicked
 hamburger.addEventListener('click', function() {
   mobileMenu.classList.add('active');
 });
 
-// Hide mobile menu when close button is clicked
 closeMenu.addEventListener('click', function() {
   mobileMenu.classList.remove('active');
 });
